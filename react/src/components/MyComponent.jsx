ddca661ccb07d6edc8f01d690ba920c7eec2b6ea
hoangson <hoangxon1209@gmail.com>
2025-02-20 13:37:06 +0700

    first commit

---
+++ react/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+ import React from "react";
+ import UseInfor from "./UseInfor";
+ class MyComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <UseInfor/>
+            </div>
+        );
+    }
+ }
+ export default MyComponent;
